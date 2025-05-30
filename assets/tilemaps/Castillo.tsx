020a8e011cb0a60964153474800922d79093cd90
indatech <jaimesmanav@gmail.com>
2025-05-29 23:45:38 -0400

    mapa nivel 2 prueba

---
+++ assets/tilemaps/Castillo.tsx
@@ -0,0 +1,472 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Castillo" tilewidth="32" tileheight="38" tilecount="156" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../textures/imagenes/wall_18.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../textures/imagenes/window_big_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../textures/imagenes/window_big_2.png" width="29" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../textures/imagenes/window_big_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../textures/imagenes/window_big_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../textures/imagenes/window_big_5.png" width="30" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../textures/imagenes/window_big_6.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../textures/imagenes/window_glass_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../textures/imagenes/window_glass_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../textures/imagenes/window_glass_tall_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../textures/imagenes/window_glass_tall_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../textures/imagenes/window_glass_tall_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../textures/imagenes/window_small_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="../textures/imagenes/window_small_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="../textures/imagenes/window_tall_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="../textures/imagenes/window_tall_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="../textures/imagenes/window_tall_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="../textures/imagenes/arch_2.png" width="32" height="19"/>
+ </tile>
+ <tile id="18">
+  <image source="../textures/imagenes/arch_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="../textures/imagenes/arch_4.png" width="21" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="../textures/imagenes/arch_5.png" width="21" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="../textures/imagenes/arch_6.png" width="18" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="../textures/imagenes/arch_7.png" width="23" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="../textures/imagenes/blank.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="../textures/imagenes/brick_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="../textures/imagenes/brick_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="../textures/imagenes/brick_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="27">
+  <image source="../textures/imagenes/brick_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="28">
+  <image source="../textures/imagenes/brick_5.png" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+  <image source="../textures/imagenes/brick_6.png" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+  <image source="../textures/imagenes/brick_7.png" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+  <image source="../textures/imagenes/brick_8.png" width="32" height="32"/>
+ </tile>
+ <tile id="32">
+  <image source="../textures/imagenes/brick_9.png" width="32" height="32"/>
+ </tile>
+ <tile id="33">
+  <image source="../textures/imagenes/brick_10.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="../textures/imagenes/brick_11.png" width="32" height="32"/>
+ </tile>
+ <tile id="35">
+  <image source="../textures/imagenes/brick_12.png" width="32" height="32"/>
+ </tile>
+ <tile id="36">
+  <image source="../textures/imagenes/brick_13.png" width="32" height="32"/>
+ </tile>
+ <tile id="37">
+  <image source="../textures/imagenes/brick_14.png" width="32" height="32"/>
+ </tile>
+ <tile id="38">
+  <image source="../textures/imagenes/brick_15.png" width="32" height="32"/>
+ </tile>
+ <tile id="39">
+  <image source="../textures/imagenes/brick_16.png" width="32" height="32"/>
+ </tile>
+ <tile id="40">
+  <image source="../textures/imagenes/brick_side_left_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="41">
+  <image source="../textures/imagenes/brick_side_left_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="42">
+  <image source="../textures/imagenes/brick_side_right_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="43">
+  <image source="../textures/imagenes/brick_side_right_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="44">
+  <image source="../textures/imagenes/ceiling_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="45">
+  <image source="../textures/imagenes/ceiling_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="46">
+  <image source="../textures/imagenes/ceiling_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="47">
+  <image source="../textures/imagenes/ceiling_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="48">
+  <image source="../textures/imagenes/ceiling_corner_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="49">
+  <image source="../textures/imagenes/ceiling_corner_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="50">
+  <image source="../textures/imagenes/ceiling_transparent_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="51">
+  <image source="../textures/imagenes/ceiling_transparent_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="52">
+  <image source="../textures/imagenes/column_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="53">
+  <image source="../textures/imagenes/column_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="54">
+  <image source="../textures/imagenes/column_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="55">
+  <image source="../textures/imagenes/column_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="56">
+  <image source="../textures/imagenes/column_5.png" width="32" height="32"/>
+ </tile>
+ <tile id="57">
+  <image source="../textures/imagenes/column_6.png" width="32" height="32"/>
+ </tile>
+ <tile id="58">
+  <image source="../textures/imagenes/column_7.png" width="32" height="32"/>
+ </tile>
+ <tile id="59">
+  <image source="../textures/imagenes/column_8.png" width="32" height="32"/>
+ </tile>
+ <tile id="60">
+  <image source="../textures/imagenes/damaged_brick_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="61">
+  <image source="../textures/imagenes/damaged_brick_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="62">
+  <image source="../textures/imagenes/damaged_brick_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="63">
+  <image source="../textures/imagenes/damaged_brick_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="64">
+  <image source="../textures/imagenes/floor_tile_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="65">
+  <image source="../textures/imagenes/floor_tile_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="66">
+  <image source="../textures/imagenes/floor_tile_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="67">
+  <image source="../textures/imagenes/floor_tile_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="68">
+  <image source="../textures/imagenes/floor_tile_carpet_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="69">
+  <image source="../textures/imagenes/floor_tile_carpet_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="70">
+  <image source="../textures/imagenes/floor_tile_carpet_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="71">
+  <image source="../textures/imagenes/floor_tile_carpet_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="72">
+  <image source="../textures/imagenes/floor_tile_carpet_corner_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="73">
+  <image source="../textures/imagenes/floor_tile_carpet_corner_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="74">
+  <image source="../textures/imagenes/floor_tile_carpet_transition_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="75">
+  <image source="../textures/imagenes/floor_tile_carpet_transition_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="76">
+  <image source="../textures/imagenes/floor_tile_corner_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="77">
+  <image source="../textures/imagenes/floor_tile_corner_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="78">
+  <image source="../textures/imagenes/floor_tile_wood_carpet_transition_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="79">
+  <image source="../textures/imagenes/floor_tile_wood_carpet_transition_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="80">
+  <image source="../textures/imagenes/floor_tile_wood_transition_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="81">
+  <image source="../textures/imagenes/floor_tile_wood_transition_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="82">
+  <image source="../textures/imagenes/floor_wood_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="83">
+  <image source="../textures/imagenes/floor_wood_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="84">
+  <image source="../textures/imagenes/floor_wood_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="85">
+  <image source="../textures/imagenes/floor_wood_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="86">
+  <image source="../textures/imagenes/floor_wood_carpet_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="87">
+  <image source="../textures/imagenes/floor_wood_carpet_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="88">
+  <image source="../textures/imagenes/floor_wood_carpet_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="89">
+  <image source="../textures/imagenes/floor_wood_carpet_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="90">
+  <image source="../textures/imagenes/floor_wood_carpet_corner_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="91">
+  <image source="../textures/imagenes/floor_wood_carpet_corner_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="92">
+  <image source="../textures/imagenes/floor_wood_carpet_transition_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="93">
+  <image source="../textures/imagenes/floor_wood_carpet_transition_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="94">
+  <image source="../textures/imagenes/floor_wood_corner_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="95">
+  <image source="../textures/imagenes/floor_wood_corner_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="96">
+  <image source="../textures/imagenes/lion_column_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="97">
+  <image source="../textures/imagenes/lion_column_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="98">
+  <image source="../textures/imagenes/lion_column_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="99">
+  <image source="../textures/imagenes/lion_column_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="100">
+  <image source="../textures/imagenes/lion_column_5.png" width="32" height="32"/>
+ </tile>
+ <tile id="101">
+  <image source="../textures/imagenes/lion_column_6.png" width="32" height="32"/>
+ </tile>
+ <tile id="102">
+  <image source="../textures/imagenes/lion_column_7.png" width="32" height="32"/>
+ </tile>
+ <tile id="103">
+  <image source="../textures/imagenes/lion_column_8.png" width="32" height="32"/>
+ </tile>
+ <tile id="104">
+  <image source="../textures/imagenes/lion_column_9.png" width="32" height="32"/>
+ </tile>
+ <tile id="105">
+  <image source="../textures/imagenes/lion_column_10.png" width="32" height="32"/>
+ </tile>
+ <tile id="106">
+  <image source="../textures/imagenes/lion_column_11.png" width="32" height="32"/>
+ </tile>
+ <tile id="107">
+  <image source="../textures/imagenes/lion_column_12.png" width="32" height="32"/>
+ </tile>
+ <tile id="108">
+  <image source="../textures/imagenes/merlons_1.png" width="32" height="13"/>
+ </tile>
+ <tile id="109">
+  <image source="../textures/imagenes/merlons_2.png" width="32" height="13"/>
+ </tile>
+ <tile id="110">
+  <image source="../textures/imagenes/merlons_3.png" width="32" height="13"/>
+ </tile>
+ <tile id="111">
+  <image source="../textures/imagenes/merlons_4.png" width="32" height="13"/>
+ </tile>
+ <tile id="112">
+  <image source="../textures/imagenes/merlons_5.png" width="32" height="13"/>
+ </tile>
+ <tile id="113">
+  <image source="../textures/imagenes/platform_1.png" width="32" height="16"/>
+ </tile>
+ <tile id="114">
+  <image source="../textures/imagenes/platform_2.png" width="32" height="16"/>
+ </tile>
+ <tile id="115">
+  <image source="../textures/imagenes/platform_3.png" width="32" height="16"/>
+ </tile>
+ <tile id="116">
+  <image source="../textures/imagenes/platform_4.png" width="32" height="16"/>
+ </tile>
+ <tile id="117">
+  <image source="../textures/imagenes/platform_shadow.png" width="32" height="6"/>
+ </tile>
+ <tile id="118">
+  <image source="../textures/imagenes/platform_wall_connection_left.png" width="16" height="16"/>
+ </tile>
+ <tile id="119">
+  <image source="../textures/imagenes/platform_wall_connection_right.png" width="16" height="16"/>
+ </tile>
+ <tile id="120">
+  <image source="../textures/imagenes/spikes.png" width="32" height="38"/>
+ </tile>
+ <tile id="121">
+  <image source="../textures/imagenes/stairs_tile_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="122">
+  <image source="../textures/imagenes/stairs_tile_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="123">
+  <image source="../textures/imagenes/stairs_tile_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="124">
+  <image source="../textures/imagenes/stairs_tile_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="125">
+  <image source="../textures/imagenes/stairs_tile_carpet_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="126">
+  <image source="../textures/imagenes/stairs_tile_carpet_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="127">
+  <image source="../textures/imagenes/stairs_tile_carpet_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="128">
+  <image source="../textures/imagenes/stairs_tile_carpet_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="129">
+  <image source="../textures/imagenes/stairs_wood_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="130">
+  <image source="../textures/imagenes/stairs_wood_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="131">
+  <image source="../textures/imagenes/stairs_wood_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="132">
+  <image source="../textures/imagenes/stairs_wood_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="133">
+  <image source="../textures/imagenes/stairs_wood_carpet_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="134">
+  <image source="../textures/imagenes/stairs_wood_carpet_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="135">
+  <image source="../textures/imagenes/stairs_wood_carpet_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="136">
+  <image source="../textures/imagenes/stairs_wood_carpet_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="137">
+  <image source="../textures/imagenes/tile_side_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="138">
+  <image source="../textures/imagenes/tile_side_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="139">
+  <image source="../textures/imagenes/wall_1.png" width="21" height="17"/>
+ </tile>
+ <tile id="140">
+  <image source="../textures/imagenes/wall_2.png" width="32" height="17"/>
+ </tile>
+ <tile id="141">
+  <image source="../textures/imagenes/wall_3.png" width="32" height="22"/>
+ </tile>
+ <tile id="142">
+  <image source="../textures/imagenes/wall_4.png" width="32" height="17"/>
+ </tile>
+ <tile id="143">
+  <image source="../textures/imagenes/wall_5.png" width="15" height="17"/>
+ </tile>
+ <tile id="144">
+  <image source="../textures/imagenes/wall_6.png" width="22" height="32"/>
+ </tile>
+ <tile id="145">
+  <image source="../textures/imagenes/wall_7.png" width="24" height="32"/>
+ </tile>
+ <tile id="146">
+  <image source="../textures/imagenes/wall_8.png" width="32" height="32"/>
+ </tile>
+ <tile id="147">
+  <image source="../textures/imagenes/wall_9.png" width="16" height="32"/>
+ </tile>
+ <tile id="148">
+  <image source="../textures/imagenes/wall_10.png" width="15" height="32"/>
+ </tile>
+ <tile id="149">
+  <image source="../textures/imagenes/wall_11.png" width="21" height="32"/>
+ </tile>
+ <tile id="150">
+  <image source="../textures/imagenes/wall_12.png" width="32" height="11"/>
+ </tile>
+ <tile id="151">
+  <image source="../textures/imagenes/wall_13.png" width="32" height="15"/>
+ </tile>
+ <tile id="152">
+  <image source="../textures/imagenes/wall_14.png" width="32" height="11"/>
+ </tile>
+ <tile id="153">
+  <image source="../textures/imagenes/wall_15.png" width="32" height="32"/>
+ </tile>
+ <tile id="154">
+  <image source="../textures/imagenes/wall_16.png" width="32" height="32"/>
+ </tile>
+ <tile id="155">
+  <image source="../textures/imagenes/wall_17.png" width="32" height="32"/>
+ </tile>
+</tileset>
